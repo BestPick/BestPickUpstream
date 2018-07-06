457e1b0e402a9bd0bccab2c706095a9c2f91ec67
khousheish <khousheish@live.com>
2018-07-06 11:38:59 +0300

    BPS-10 Generate New Home-Phones Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { HomePhonesComponent } from './home-page/home-phones/home-phones.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomePageComponent
+        HomePageComponent,
+        HomePhonesComponent
     ],
     imports: [
         BrowserModule
---
+++ src/app/home-page/home-phones/home-phones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-phones',
+  templateUrl: './home-phones.component.html',
+  styleUrls: ['./home-phones.component.scss']
+})
+export class HomePhonesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
