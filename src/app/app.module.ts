2676d0adb057712024999bd615877661c9214d92
Mohieddine Zahwi <mohieddinezahwi@Mohieddines-MacBook-Pro-2.local>
2019-01-08 22:30:00 +0200

    BPS-12 SCSS-Lint Fixes and smartphones fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { SizeComponent } from './size/size.component';
+import { BestPickComponent } from './best-pick/best-pick.component';
 import { BrandComponent } from './brand/brand.component';
 import { BudgetComponent } from './budget/budget.component';
-import { BestPickComponent } from './best-pick/best-pick.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SizeComponent } from './size/size.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomePageComponent,
-        SizeComponent,
-        BrandComponent,
-        BudgetComponent,
-        BestPickComponent,
-    ],
-    imports: [
-        BrowserModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    SizeComponent,
+    BrandComponent,
+    BudgetComponent,
+    BestPickComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [
+
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
 })
+
 export class AppModule { }
